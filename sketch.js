6a43ff7f8bf53d455d666171cf1695a197d8fc8f
Tristan <99270772+thaeger1@users.noreply.github.com>
2024-04-09 14:48:51 -0400

    Progress

--- sketch.js
+++ sketch.js
@@ -1,13 +1,4 @@
 
-
-function setup() {
-  createCanvas(_width, _height);
-  fill(255);
-  noStroke();
-
-  frameRate(30);
-}
-
 let _width = 800;
 let _height = 600;
 
@@ -17,66 +8,142 @@ let rect_h = 300;
 let pv = [];        // list of vectors that represent our particles
 let pr = [];        // r is position, v is velocity
 
-let grid = [];      // 2d array for our grid, store as collocated, treat as staggered
+let objGrid = [];   // 2d array for our grid, store as collocated, treat as staggered
+let velGrid = [];
+
 let cellSize = 10;  // keep rect width/height in mind when adjusting
 
-let time_buff = 10;
+let time_buff = 10; // increase for more "startup" time
+
+function setup() {
+  createCanvas(_width, _height);
+  fill(255);
+  noStroke();
+
+  frameRate(30);
+
+  // init objGrid here
+  initObjGrid(objGrid);
+  // init velGrid here
+  initVelGrid(velGrid);
+  // init particles
+  initParticles(num_ptc);
+}
 
 function draw() {
-  let dt = frameRate();
+  // get out frameRate, as we will be using this for our dt in our physics sim
+  let dt = 1/frameRate();
   if (frameCount < time_buff) return; 
+
   background(228,202,159); // #savebeige
+  fill(0); text("fr: " + str(floor(frameRate())), _width - 50, 25);      // frame rate text
+  fill(25); rect((_width-rect_w)/2, (_height-rect_h)/2, rect_w, rect_h); // draw tank
+  // add rotation by angle theta, will require reworking forces applied in particle step
 
-  // frame rate text
-  fill(0); text("fr: " + str(floor(frameRate())), _width - 50, 25);
+  /// PHYSICS SIM ///
 
-  // draw tank
-  fill(25); rect((_width-rect_w)/2, (_height-rect_h)/2, rect_w, rect_h);
-  // add rotation by angle theta
-
-  // simulate particles
+  // simulate particles //
   for (let i = 0; i < num_ptc; i++) {
     updateParticle(i, dt);
   }
 
-  // particles -> grid : velocities
-  for (let i = 0; i < floor(_width/cellSize); i++) {
-    grid[i] = [];
-    for (let j = 0; j < floor(_height/cellSize); j++) {
-      grid[i][j] = 0; // we want 0 for water, 1 for walls, and und for air
-    }
-  }
-  // read velocities in using cell size and positions
+  //--
+  // particles -> grid : velocities //
 
-  // make velocities incompressible
+  // updateObjGrid() to find where water is
+  // readToGrid() all of our particles
 
-  // grid -> particles : velocities
+  //--
+  // make velocities incompressible //
 
+  // ForceIncompress() to update velGrid
 
+  //--
+  // grid -> particles : velocities //
 
-  // our outline //
-  
-  // go over particles to update velocities
-    // particles :)
+  // readFromGrid() to particles
 
-  // normalize pressures
-    // calc divergence
-    // calc second grid
-      // FLIP: copy grid
+  /// END PHYSICS SIM ///
 
-  // update velocities of particles
-  // FLIP use difference of grids to update particles
+  // draw all of our particles
 }
 
-function initParticles(num_ptc) {
-  // for num particles
-    // pr = [new_x, new_y]
-    // pv = [0,0]
+function readToGrid(velGrid, pv, pr) {
+  // for i in range len(pv or pr)
+  for (let i = 0; i < num_ptc; i++) {
+    // find what cell we are in
+    let cell_x = floor(pr[i].x / cellSize);
+    let cell_y = floor(pr[i].y / cellSize);
+    console.log(pr, cell_x, cell_y);
+
+    // find lx, ly, local x,y coords within the cell
+    // let lx = pr[i].x - cell_x*cellSize;
+    // let ly = pr[i].y - cell_y*cellSize;
+    
+    // read velocities in
+    // make 4 vectors around the cell, 
+      // u_i,j
+      // v_i,j
+      // u_i+1,j
+      // v_i,j+1
+  }
+}
+
+// TODO
+function ForceIncompress(velGrid) {
+  // where we calculate divergence
+}
+
+function readFromGrid(velGrid, pv, pr) {
+  // give particles velocity based on which grid cell they are in
 }
 
 function updateParticle(index, dt) {
   // update velocity at index
+
+  // pv[index].Add() // add might save some time
+  // new p5.Vector(vx + g*dt, vy + g*dt);
   // v = v + g * dt
+  
   // update position at index
+  //
   // r = r + dt * v
+}
+
+// TODO
+function updateObjGrid(objGrid) {
+  // clear (inside of) grid and replace with 0s (air)
+  // go over every particle, mark cell particle is in as water (1) as long as its not a wall
+}
+
+function initParticles(num_ptc) {
+  // for num particles
+  for (let i = 0; i < num_ptc; i++) {
+    let _x = random((_width-rect_w)/2 + 1, (_width+rect_w)/2 - 1);
+    let _y = random((_height-rect_h)/2 + 1, (_height+rect_h)/2 - 1);
+    pr[i] = new p5.Vector(_x,_y); // random x,y
+    pv[i] = new p5.Vector(0,0);   // no starting velocity
+  }
+}
+
+function initVelGrid(velGrid) {
+  for (let i = 0; i < floor(_width/cellSize); i++) {
+    velGrid[i] = [];
+    for (let j = 0; j < floor(_height/cellSize); j++) {
+      velGrid[i][j] = new p5.Vector(0,0); // we want 0 for water, 1 for walls, and undef for air
+    }
+  }
+}
+
+function initObjGrid(objGrid) {
+  for (let i = 0; i < floor(_width/cellSize); i++) {
+    objGrid[i] = [];
+    for (let j = 0; j < floor(_height/cellSize); j++) {
+      if (i == 0 || j == 0 || i == _width - 1 || j == _height - 1) {
+        objGrid[i][j] == 2;
+      } else {
+        objGrid[i][j] = 0; // we want 0 for air, 1 for water, and 2 for walls
+      }
+    }
+  }
 }
